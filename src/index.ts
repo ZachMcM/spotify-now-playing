a34252b2ef11974f76d500fe606617655dfdbe6f
Zach McMullen <zachmcmullen04@gmail.com>
2024-06-02 17:37:01 -0700

    build fail on digital ocean fix

--- src/index.ts
+++ src/index.ts
@@ -25,4 +25,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+});
